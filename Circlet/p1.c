8e9de106d68fc4e2e2f56ea5cd5300ef3bc7b159
Anshupaija <paijaanshu223@gmail.com>
2024-05-28 15:34:21 +0530

    .

---
+++ Circlet/p1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 41; j < 41 + i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
