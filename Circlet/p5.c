63f0d4d37a3c56d83e1fd8d4a515d5d9de5537e1
Anshupaija <paijaanshu223@gmail.com>
2024-05-28 15:39:02 +0530

    .

--- Circlet/p5.c
+++ Circlet/p5.c
@@ -3,9 +3,12 @@
 int main() {
     for (int i = 5; i >= 1; i--) {
         for (int j = 1; j < i; j++) {
-            printf("   "); 
+            printf("   ");
         }
         for (int j = i; j <= 5; j++) {
+            printf("%d ", j);
+        }
+        for (int j = 5 - 1; j >= i; j--) {
             printf("%d ", j);
         }
         printf("\n");
