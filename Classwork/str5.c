414b77f1a32a21cb7cdc3c34ad28bdbbdfd4c051
Anshupaija <paijaanshu223@gmail.com>
2024-05-29 17:01:01 +0530

    .

--- Classwork/str5.c
+++ Classwork/str5.c
@@ -1,13 +1,27 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//    char str1[20] = "Hello ";
+//    char str2[20] = "World";
+
+//    strcat(str1, str2);
+
+//    printf("Concatenated string: %s\n", str1);
+
+//    return 0;
+// }
 #include <stdio.h>
 #include <string.h>
 
 int main() {
    char str1[20] = "Hello ";
    char str2[20] = "World";
+   char joined[40];
 
-   strcat(str1, str2);
+   strcpy(joined,strcat(str1,str2));
 
-   printf("Concatenated string: %s\n", str1);
+   printf("Concatenated string: %s\n", joined);
 
    return 0;
 }
---
+++ Classwork/str6.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// #include<string.h>
+
+// int main(){
+//     char n[100]="Anshu";
+//     printf("%s",strrev(n));
+// }
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char n[100]="A";
+    char n1[100]="a";
+    printf("%d",strcmp(n,n1));
+}
